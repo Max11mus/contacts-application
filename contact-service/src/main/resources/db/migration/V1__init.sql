035a2c093e7c1566377dd9a69d5066c88285a67c
max11mus <koseymax@gmail.com>
2023-07-06 16:31:28 +0300

    Defined unique columns in contact table

--- contact-service/src/main/resources/db/migration/V1__init.sql
+++ contact-service/src/main/resources/db/migration/V1__init.sql
@@ -4,6 +4,6 @@ CREATE TABLE IF NOT EXISTS public.contact (
     contact_id uuid NOT NULL,
     contact_name varchar(100) NOT NULL,
     owner_id uuid NOT NULL,
-    CONSTRAINT contact_contact_name_key UNIQUE (contact_name),
+    CONSTRAINT contact_contact_name_owner_id UNIQUE (contact_name, owner_id),
     CONSTRAINT contact_pkey PRIMARY KEY (contact_id)
     );
